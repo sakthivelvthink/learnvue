4ae4b4baa092584a18d5d8075cdb572b0e53dcc8
Satikant <satikant@vthink.co.in>
2020-11-30 12:49:06 +0530

    Added Vue basics

---
+++ vuebasics/src/components/Skills.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="skills">
+   <h1 v-once>{{name}}</h1>
+
+    <!-- Add this -->
+    {{ btnState ? 'The button is disabled' : 'The button is active'}}
+    <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+ data() {
+    return {
+      name: 'Sakthivel',
+      btnState: true,
+      skills: [
+          { "skill": "Vue.js" },
+          { "skill": "Frontend Developer" }
+      ]
+    }
+  }
+}
+</script>
+
+<style  scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
