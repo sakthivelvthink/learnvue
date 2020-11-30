b68f838e86b8bf9e009838b455e97638a8fe741e
Satikant <satikant@vthink.co.in>
2020-11-30 17:11:00 +0530

    Added Routing

--- vuenewapp/src/App.vue
+++ vuenewapp/src/App.vue
@@ -1,16 +1,18 @@
 <template>
-<div class="app">
-  <Skills/>
+ <div id="app">
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Skills from './components/Skills'
 
 export default {
   name: 'App',
   components: {
-    Skills
   }
 }
 </script>
@@ -32,5 +34,18 @@ body, html {
 }
 #app {
     width: 50%;
+}
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
 }
 </style>
---
+++ vuenewapp/src/components/About.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+ data() {
+    return {
+      name: 'Sakthivel',
+      btnState: true,
+      skills: [
+          { "skill": "Vue.js" },
+          { "skill": "Frontend Developer" }
+      ]
+    }
+  },
+   methods : {
+      addSkill(){
+          this.skills.push({skill: this.skill});
+          this.skill = '';
+      }
+  }
+}
+</script>
+
+<style  scoped>
+ .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+</style>
