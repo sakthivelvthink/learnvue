7da1bfcd79cfb8c5ce0b712692920fdf1f23433e
Satikant <satikant@vthink.co.in>
2020-11-30 15:19:13 +0530

    added new project

---
+++ vuenewapp/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/Skills'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+</style>
