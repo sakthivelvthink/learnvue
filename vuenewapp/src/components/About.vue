452fa35b6ea990ff74435c5c8c1e1118bc4e1787
Satikant <satikant@vthink.co.in>
2020-12-01 12:17:54 +0530

    Added vuex Frame work

--- vuenewapp/src/components/About.vue
+++ vuenewapp/src/components/About.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="about">
-    <h1>This is an about page</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+    <h1>TODOS</h1>
+    <TodosList/>
   </div>
 </template>
 
 <script>
+import TodosList from "./TodosList.vue"
 export default {
   name: 'About',
+  components: {
+    TodosList
+  },
  data() {
     return {
       name: 'Sakthivel',
---
+++ vuenewapp/src/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+   
+    },
+    mutations: {
+     
+     
+    }
+})
+}
+
+export default createStore
