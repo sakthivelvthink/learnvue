3521caf19f0a8d64c83915e6854e7dbdb9f724b1
Satikant <satikant@vthink.co.in>
2021-01-04 16:53:05 +0530

    added Upload file

--- vuenewapp/src/router.js
+++ vuenewapp/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Skills from './components/Skills.vue'
 import About from './components/About.vue'
 import LoginComponent from './components/LoginComponent'
+import uploadMedia from './components/uploadMedia'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: LoginComponent
+    },
+    {
+      path: '/uploadMedia',
+      name: 'uploadMedia',
+      component: uploadMedia
     }
   ]
 })
